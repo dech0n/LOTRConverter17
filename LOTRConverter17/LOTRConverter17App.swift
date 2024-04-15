013d4da7c9f3ff8a9a4d6391092eb92c12649bf7
Dechon <72828058+dech0n@users.noreply.github.com>
2024-04-15 13:49:22 -0700

    Initial Commit

---
+++ LOTRConverter17/LOTRConverter17App.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverter17App.swift
+//  LOTRConverter17
+//
+//  Created by Dechon Ryan on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverter17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
