2397a347d7f46ac7d1507724e1d907b122f57c47
Dechon <72828058+dech0n@users.noreply.github.com>
2024-04-17 11:27:49 -0700

    Create instances for all exchange rates

--- LOTRConverter17/ExchangeInfo.swift
+++ LOTRConverter17/ExchangeInfo.swift
@@ -29,7 +29,13 @@ struct ExchangeInfo: View {
                 
                 // exchg rates
                 
-                ExtractedView()
+                ExchangeRate(leftImage: .goldpiece, rightImage: .goldpenny, text: "1 Gold Piece = 4 Gold Pennies")
+                
+                ExchangeRate(leftImage: .goldpenny, rightImage: .silverpiece, text: "1 Gold Penny = 4 Silver Pieces")
+                
+                ExchangeRate(leftImage: .silverpiece, rightImage: .silverpenny, text: "1 Silver Piece = 4 Silver Pennies")
+                
+                ExchangeRate(leftImage: .silverpenny, rightImage: .copperpenny, text: "1 Silver Piece = 100 Copper Pennies")
                 
                 // Done button
                 Button("Done") {
