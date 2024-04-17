0c416fed814ab0cbd187734714b641fe78f071b7
Dechon <72828058+dech0n@users.noreply.github.com>
2024-04-17 13:12:00 -0700

    Add background and basic stack structure

---
+++ LOTRConverter17/SelectCurrency.swift
@@ -0,0 +1,52 @@
+//
+//  SelectCurrency.swift
+//  LOTRConverter17
+//
+//  Created by Dechon Ryan on 4/17/24.
+//
+
+import SwiftUI
+
+struct SelectCurrency: View {
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        ZStack {
+            // parchment background image
+            Image(.parchment)
+                .resizable()
+                .ignoresSafeArea()
+                .background(.brown)
+    
+            VStack {
+                // text
+                Text("Select the currency you are starting with:")
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                
+                // currency icons
+                
+                // text
+                Text("Select the currency you would like to convert to:")
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                
+                // currency icons
+                
+                // done button
+                Button("Done") {
+                    dismiss()
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.brown)
+                .font(.largeTitle)
+                .padding()
+                .foregroundStyle(.white)
+            }
+            .padding()
+            .multilineTextAlignment(.center)
+        }
+    }
+}
+
+#Preview {
+    SelectCurrency()
+}
