afcadf43f90655dfacb871e179bde5161b002622
Dechon <72828058+dech0n@users.noreply.github.com>
2024-04-17 10:53:58 -0700

    Build basic structure of view

---
+++ LOTRConverter17/ExchangeInfo.swift
@@ -0,0 +1,69 @@
+//
+//  ExchangeInfo.swift
+//  LOTRConverter17
+//
+//  Created by Dechon Ryan on 4/17/24.
+//
+
+import SwiftUI
+
+struct ExchangeInfo: View {
+    var body: some View {
+        ZStack {
+            // background parchment image
+            Image(.parchment)
+                .resizable()
+                .ignoresSafeArea()
+                .background(.brown)
+            
+            VStack {
+                // Title text
+                Text("Exchange Rates")
+                    .font(.largeTitle)
+                    .tracking(3)
+                
+                // desc text
+                Text("Here at the Prancing Pony, we are happy to offer you a place where you can exchange all the known currencies in the entire world except one. We used to take Brandy Bucks, but after finding out that it was a person instead of a piece of paper, we realized it had no value to us. Below is a simple guide to our currency exchange rates:")
+                    .font(.title2)
+                    .padding()
+                
+                // exchg rates
+                
+                HStack {
+                    // left currency image
+                    Image(.goldpiece)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 33)
+                    
+                    // exchg rate text
+                    Text("1 Gold Piece = 4 Gold Pennies")
+                    
+                    // right currency image
+                    Image(.goldpenny)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 33)
+                }
+                
+                // Done button
+                Button("Done") {
+                    
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.brown)
+                .font(.largeTitle)
+                .padding()
+                .foregroundStyle(.white)
+                
+            }
+            .foregroundStyle(.black)
+        }
+            // Vstack
+                // some Hstacks
+    }
+}
+
+#Preview {
+    ExchangeInfo()
+}
