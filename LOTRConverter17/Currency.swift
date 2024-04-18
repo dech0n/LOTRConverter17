a936e2db2e088268d133a172f4c92da59a4b75bb
Dechon <72828058+dech0n@users.noreply.github.com>
2024-04-18 10:31:55 -0700

    Build Currency enum

---
+++ LOTRConverter17/Currency.swift
@@ -0,0 +1,45 @@
+//
+//  Currency.swift
+//  LOTRConverter17
+//
+//  Created by Dechon Ryan on 4/17/24.
+//
+import SwiftUI
+
+enum Currency: Double {
+    case copperPenny = 6400
+    case silverPenny = 64
+    case silverPiece = 16
+    case goldPenny = 4
+    case goldPiece = 1
+    
+    var image: ImageResource { // computed property; always need type declared
+        switch self {
+        case .copperPenny:
+                .copperpenny
+        case .silverPenny:
+                .silverpenny
+        case .silverPiece:
+                .silverpiece
+        case .goldPenny:
+                .goldpenny
+        case .goldPiece:
+                .goldpiece
+        }
+    }
+    
+    var name: String {
+        switch self {
+        case .copperPenny:
+            "Copper Penny"
+        case .silverPenny:
+            "Silver Penny"
+        case .silverPiece:
+            "Silver Piece"
+        case .goldPenny:
+            "Gold Penny"
+        case .goldPiece:
+            "Gold Piece"
+        }
+    }
+}
